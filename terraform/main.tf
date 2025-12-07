b997cba364ccbc98faabc88759e20d83aa89a45c
onelngoma-source <onelngoma@gmail.com>
2025-12-08 00:56:42 +0100

    aaaaaaaa

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,24 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.7.0"
+      version = "~> 3.6.0"
     }
   }
+  required_version = ">= 1.5.0"
 }
 
 provider "docker" {}
 
 resource "docker_image" "site" {
   name = "onelmelvy/devops-site:latest"
+  build {
+    context = "../" # dossier racine du projet contenant Dockerfile
+  }
 }
 
 resource "docker_container" "site" {
   name  = "devops-site-container"
-  image = docker_image.site.image_id
+  image = docker_image.site.latest
 
   ports {
     internal = 80
