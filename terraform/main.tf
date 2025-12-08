a4d6734c1d6950182ebc9954d4258ca67541c0a0
onelngoma-source <onelngoma@gmail.com>
2025-12-08 09:05:07 +0100

    aaaaaaaaaa

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# 1. On définit les outils nécessaires (Le Provider Docker)
 terraform {
   required_providers {
     docker = {
@@ -7,18 +8,31 @@ terraform {
   }
 }
 
+# 2. On connecte Terraform à ton Docker local
 provider "docker" {}
 
-resource "docker_image" "site" {
-  name = "onelmelvy/devops-site:latest"
+# 3. L'Image : On demande à Terraform de récupérer l'image Nginx légère
+resource "docker_image" "nginx" {
+  name         = "nginx:alpine"
+  keep_locally = false
 }
 
-resource "docker_container" "site" {
-  name  = "devops-site-container"
-  image = docker_image.site.image_id
+# 4. Le Conteneur : On configure le serveur
+resource "docker_container" "nginx_server" {
+  image = docker_image.nginx.image_id
+  name  = "devops-site-con" # Le nom que tu verras dans Docker Desktop
 
+  # On mappe le port 8000 de ton PC vers le port 80 du conteneur
   ports {
     internal = 80
-    external = 8080
+    external = 8000
+  }
+
+  # C'est la partie magique : on remplace le fichier index.html du conteneur
+  # par TON fichier index.html local.
+  volumes {
+    container_path = "/usr/share/nginx/html/index.html"
+    host_path      = "${path.cwd}/index.html"
+    read_only      = true
   }
 }
